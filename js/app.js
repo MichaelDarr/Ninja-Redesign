15c42a852cc1e817cdeb8961ca191c32d78016b8
Michael Darr <michaeldarr45@gmail.com>
2014-08-07 13:48:05 -0400

    first push

--- js/app.js
+++ js/app.js
@@ -1,3 +1,76 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		anchors:['about', 'portfolio', 'contact'],
+	});
+	$('.tlt').textillate({
+		in: {
+			effect: 'flipInY',
+			delay: 10,
+		},
+	});
+	$("#navigation").addClass("pageLoaded");
+	about = false;
+	portfolio = false;
+	contact = false;
+});
+
+$(function() {
+  $(".aboutIcon")
+  .mouseenter(function() {
+  	if(!about) {
+	  	animateText('about')
+			about = true;
+  	}
+    $('#aboutMessage').textillate('in');
+  })
+  .mouseleave(function() {
+  	$('#aboutMessage').textillate('out');
+  });
+});
+
+$(function() {
+  $(".portfolioIcon")
+  .mouseenter(function() {
+  	if(!portfolio) {
+	  	animateText('portfolio')
+			portfolio = true;
+  	}
+    $('#portfolioMessage').textillate('in');
+  })
+  .mouseleave(function() {
+  	$('#portfolioMessage').textillate('out');
+  });
+});
+
+$(function() {
+  $(".contactIcon")
+  .mouseenter(function() {
+  	if(!contact) {
+	  	animateText('contact')
+			contact= true;
+  	}
+    $('#contactMessage').textillate('in');
+  })
+  .mouseleave(function() {
+  	$('#contactMessage').textillate('out');
+  });
+});
+
+function animateText(id, isSet){
+	if (!isSet) {
+		$('#' + id + 'Message').textillate({
+			in: {
+				effect: 'fadeInLeft',
+				delay: 20,
+			},
+			out: {
+				effect: "bounceOutRight",
+				delay: 20,
+			},
+		});
+	}
+}
